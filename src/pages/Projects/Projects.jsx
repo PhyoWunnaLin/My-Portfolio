aa8d698fb0d42f3f2dd798cb093d48f3b5699d33
PhyoWunnaLin <phyowunnalin1214@gmail.com>
2023-09-05 14:20:40 +0630

    update

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import MainLayout from "../../Layouts/MainLayout";
-import edu from "../../assets/projectsPhoto/edu.png";
+import edu from "../../assets/projectsPhoto/edu2.png";
 import dashboard from "../../assets/projectsPhoto/dashboard.png";
 import contact from "../../assets/projectsPhoto/contact.png";
+import pos from "../../assets/projectsPhoto/pos.png";
 import "./Projects.css";
 import { Link } from "react-router-dom";
 
@@ -26,12 +27,12 @@ const Projects = () => {
           <div className="flex gap-14 flex-wrap mt-3">
             <Link
               to={"https://eduvide.netlify.app"}
-              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525] rounded"
+              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525]"
             >
-              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden rounded-t">
+              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden">
                 <img
                   src={edu}
-                  className="custom-hover duration-500 w-full h-full object-cover object-top"
+                  className="custom-hover duration-500 w-full h-full object-top"
                 />
               </div>
               <h1 className="text-[#323232] dark:text-white mx-5 text-xl font-bold tracking-wider">
@@ -90,9 +91,9 @@ const Projects = () => {
             </Link>
             <Link
               to={"https://edu-pro-dashboard2.netlify.app"}
-              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525] rounded"
+              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525]"
             >
-              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden rounded-t">
+              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden">
                 <img
                   src={dashboard}
                   className="custom-hover duration-500 w-full h-full object-cover object-top"
@@ -156,9 +157,9 @@ const Projects = () => {
             </Link>
             <Link
               to={"https://contact-web1.netlify.app"}
-              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525] rounded"
+              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525]"
             >
-              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden rounded">
+              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden">
                 <img
                   src={contact}
                   className="custom-hover duration-500 w-full h-full object-cover object-top"
@@ -214,6 +215,73 @@ const Projects = () => {
                 </div>
                 <div>
                   <Link to={"https://github.com/PhyoWunnaLin/Contact-web"}>
+                    <button className=" px-10 py-4 bg-black dark:bg-white text-white dark:text-black font-medium tracking-wider hover:tracking-[0.15em] duration-500">
+                      Git
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </Link>
+            <Link
+              to={"https://webpos1.netlify.app"}
+              className="card cursor-pointer flex flex-col gap-5 w-[45%] max-lg:w-[100%] bg-white dark:bg-[#323232] shadow-lg dark:shadow-[#252525]"
+            >
+              <div className="w-full h-[250px] max-lg:h-[500px] max-[830px]:h-[450px] max-md:h-[400px] max-sm:h-[300px] max-[500px]:h-[250px] max-[400px]:h-[200px] max-[350px]:h-[170px] max-[300px]:h-[150px] overflow-hidden">
+                <img
+                  src={pos}
+                  className="custom-hover duration-500 w-full h-full object-top"
+                />
+              </div>
+              <h1 className="text-[#323232] dark:text-white mx-5 text-xl font-bold tracking-wider">
+                Web POS
+              </h1>
+              <hr className="border-t border-[#EEEEEE] dark:border-[#1f1e1e] mx-5" />
+              <div className="flex gap-1 flex-wrap mx-5 italic">
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  React
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  React-Router-Dom
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  Tailwind
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  React-icons
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  RTK-Query
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  API
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  React-Hot-Toast
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  sweetalert2
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  Mantine
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  CSS
+                </p>
+                <p className="mr-5 text-[#323232] dark:text-[#adadad] font-medium tracking-widest">
+                  Cookie
+                </p>
+              </div>
+              <hr className="border-t border-[#EEEEEE] dark:border-[#1f1e1e] mx-5" />
+              <div className="flex mx-5 mb-5 max-[400px]:justify-between flex-wrap gap-5">
+                <div className="w-[150px]">
+                  <Link to={"https://webpos1.netlify.app"}>
+                    <button className=" px-10 py-4 bg-black dark:bg-white text-white dark:text-black font-medium tracking-wider hover:tracking-[0.15em] duration-500">
+                      Demo
+                    </button>
+                  </Link>
+                </div>
+                <div>
+                  <Link to={"https://github.com/PhyoWunnaLin/Web-Pos"}>
                     <button className=" px-10 py-4 bg-black dark:bg-white text-white dark:text-black font-medium tracking-wider hover:tracking-[0.15em] duration-500">
                       Git
                     </button>
